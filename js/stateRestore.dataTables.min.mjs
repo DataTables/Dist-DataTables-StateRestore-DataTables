46f20ec00daf03be0a6139b43c96860519404870
dtbuild <dtbuild@rigil>
2026-09-25 14:05:50 +0000

    b7953fb6abcacb678c36ae39847ac92861242ad1 Release 2.1.1
    Sync to source repo @b7953fb6abcacb678c36ae39847ac92861242ad1

--- js/stateRestore.dataTables.min.mjs
+++ js/stateRestore.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! StateRestore default styling 2.1.0 for DataTables
+/*! StateRestore default styling 2.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-dt";import StateRestore from"datatables.net-staterestore";export default DataTable;
